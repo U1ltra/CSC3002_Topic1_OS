0605edc6ca978f9cff6352b98e421b01e400303c
U1ltra <59546086+U1ltra@users.noreply.github.com>
2020-05-08 20:26:36 +0800

    scheduling_algorithms_20/5/8
    removed redundent tastStruc class.

--- scheduling_algo.cpp
+++ scheduling_algo.cpp
@@ -1,20 +1,131 @@
 
 /* File: scheduling_algo.h
  * -----------------------
- * This file simulates FCFS, SPN, SJF, SRT, HRRN, RR, MFQ,
- * FSS task scheduling algorithms
+ * This file implements scheduling class.
  */
 
 #include "scheduling_algo.h"
-#include "taskstuc.h"
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector<taskStuc> PROCESSES;
 
+
+scheduling::scheduling(){
+    selectAlgo();
+    if (algorithm == _FCFS) setFCFS();
+    Twait = 0;
+    TResp = 0;
+    TCy = 0;
+}
+
+bool scheduling::cmpArrivalq(task* a, task* b){
+    if (a->arrivalq == b->arrivalq) return a->pid < b->pid;
+    return  a->arrivalq < b->arrivalq;
+}
+
+bool scheduling::cmpPriority(task* a, task* b){
+    if (a->priority == b->priority) return a->pid < b->pid;
+    return a->priority < b->priority;
+}
+
+bool scheduling::cmpArrivalt(task* a, task* b){
+    if (a->arrivaltime == b->arrivaltime) return a->pid < b->pid;
+    return a->arrivaltime < b->arrivaltime;
+}
+
+bool scheduling::cmpwaitT(task* a, task* b){
+    if (a->waitT == b->waitT) return a->pid < b->pid;
+    return a->waitT < b->waitT;
+}
+
+bool scheduling::cmpRemain(task* a, task* b){
+    if (a->timeRemain == b->timeRemain) return a->pid < b->pid;
+    return a->timeRemain < b->timeRemain;
+}
+
+bool scheduling::comparision(task * a, task * b){
+    if (algorithm == 0) return cmpArrivalt(a, b);
+}
+
+void scheduling::enqueue(task * a){
+    int position = pqueue.size();
+    pqueue.push_back(a);
+    if (position == 0) return;
+
+    task* parent = pqueue[(position-1)/2];
+    while (comparision(a, parent)){
+        task* buffer;
+        buffer = parent;
+        pqueue[(position-1)/2] = a;
+        pqueue[position] = parent;
+        position = (position-1)/2;
+        if (position==0) break;
+        parent = pqueue[(position-1)/2];
+    }
+}
+
+void scheduling::selectAlgo(){
+    int index;
+    cout << "Enter the scheduling algorithm (int number from [0,7]) >>> ";
+    cin >> index;
+    algorithm = (schedulingAlgos) index;
+}
+
+void scheduling::setFCFS(){
+    int nPR; task *a; int buffer;
+    cout << "Please enter the total number of processes >>> ";
+    cin >> nPR;
+    cout << "Process    Brust Time    Arrival Time (moment)" << endl;
+    cout << "-------    ----------    ---------------------" << endl;
+    for (int i=0; i<nPR; i++)                                   // set pid and brustime for each process
+    {
+        a->pid = i;
+        cout << "   P" << i+1 << "          " << endl;
+        cin >> buffer;
+        a->timeRemain = buffer;
+        cout << "                " << endl;
+        cin >> buffer;
+        a->arrivaltime = buffer;
+        enqueue(a);
+    }
+}
+
+void scheduling::efficiency(){                                      // need to later change to make operations on individual task distributions
+    int max=-1, min=-1, maxC=-1, minC=-1;
+    for (size_t i=1; i<pqueue.size(); i++) {
+        if (pqueue[i]->arrivaltime < pqueue[i-1]->timeRemain){
+            int waitTime = pqueue[i-1]->timeRemain - pqueue[i]->arrivaltime;
+            Twait += waitTime;
+            TCy += waitTime + pqueue[i]->timeRemain;                // total cycling time
+            if (max==-1 || max < waitTime)  max = waitTime;
+            if (min==-1 || min > waitTime)  min = waitTime;
+            if (maxC==-1 || maxC < waitTime + pqueue[i]->timeRemain)    maxC = waitTime + pqueue[i]->timeRemain;
+            if (minC==-1 || minC > waitTime + pqueue[i]->timeRemain)    minC = waitTime + pqueue[i]->timeRemain;
+        }
+        else TCy += pqueue[i]->timeRemain;
+    }
+
+    TResp = Twait;
+    AverWait = AverResp = Twait / pqueue.size();
+    MaxWait = MaxResp = max;
+    MinWait = MinResp = min;
+    AverCy = TCy / pqueue.size();
+
+}
+
+int scheduling::FCFS(){
+    exeQ = pqueue;                              // for FCFS, the execution queue is jus the same as task queue sorted according to arriveq
+}
+
+
+
+
+
+
+/*
 int FCFS(){
     int nPR; taskStuc a; int buffer; string press;
     cout << "Please enter the total number of processes >>> ";
@@ -45,6 +156,13 @@ int FCFS(){
 
     PROCESSES.clear();          // clear the global vector when one simulation is done
 }
+
+
+int avgWaitingT(){
+
+}
+*/
+
 
 
 
--- scheduling_algo.h
+++ scheduling_algo.h
@@ -1,35 +1,186 @@
 
-/* File: schedule_algo.h
+/*
+ * File: schedule_algo.h
  * ---------------------
- * This file exports several traditional task scheduling algorithm.
+ * This file exports a sheduling simulation class where several traditional
+ * task scheduling algorithms are defined.
  */
 
 #ifndef SCHEDULING_ALGO_H
 #define SCHEDULING_ALGO_H
 
-// First-Come First-Served Scheduling //
-int FCFS();
+#include <string>
+#include <vector>                           // use vector for now -- remember to revise to dynamic array
 
-// Shortest-Job First Scheduling //
-int SJF();
 
-// Shortest-Process Next Scheduling //
-int SPN();
+/*
+ * Type: schedulingAlgos
+ * ---------------------
+ * This type respresents the eight scheduling algorithms.
+ */
+enum schedulingAlgos {_FCFS, _SJF, _SPN, _SRT, _HRRN, _RR, _MFQ, _FSS};
 
-// Shortest- Remaining Time Scheduling //
-int SRT();
+/*
+ * Class: scheduling
+ * -----------------
+ * This class models a specific period of task scheduling algorithms
+ * of the operating system. Pros and cons of different scheduling
+ * algorithms could be revealed by comparing obtained efficiency
+ * determinants from the simulation.
+ */
+class scheduling{
+public:
 
-// Highest-Response Ratio Next Scheduling //
-int HRRN();
+    // constructors //
+    scheduling();
+    scheduling(const scheduling &) = default;   // default for now
 
-// Round_Robin Scheduling //
-int RR();
+    // destructor //
+    ~scheduling() = default;                // release memory on heap -- later when dynamic array become the implementation of BST, for now just simply use vector //
 
-// Multipule-Feedback Queque Scheduling //
-int MFQ();
+    /*
+     * determine scheduling algo to be used
+     */
+    void selectAlgo();
 
-// Fair-Sharing Scheduling //
-int FSS();
+    /*
+     * set the tasks and their attributes to be scheduled, according
+     * to the algorithm selected.
+     */
+    void setFCFS();                         // maybe i should condition them inside one <code>set</code> instead //
+    void setSJF();                          // leave it for now //
+    void setSPN();
+    void setSRT();
+    void setHRRN();
+    void setRR();
+    void setMFQ();
+    void setFSS();
+
+
+    /*
+     * draw ganttchart
+     */
+    void gantt();
+
+
+    /////////////////////////////
+    /// Scheduling Algorithms ///
+    /////////////////////////////
+
+    // First-Come First-Served Scheduling //
+    int FCFS();
+
+    // Shortest-Job First Scheduling //
+    int SJF();
+
+    // Shortest-Process Next Scheduling //
+    int SPN();
+
+    // Shortest- Remaining Time Scheduling //
+    int SRT();
+
+    // Highest-Response Ratio Next Scheduling //
+    int HRRN();
+
+    // Round_Robin Scheduling //
+    int RR();
+
+    // Multipule-Feedback Queque Scheduling //
+    int MFQ();
+
+    // Fair-Sharing Scheduling //
+    int FSS();
+
+
+private:
+
+
+    // structure of a individual task/process created //
+
+    struct task{
+        std::string taskName;
+        int pid;                // identity of tasks, which is according to the creating order of the task. basically is just for reference
+        int timeRemain;         // time needed for the task to finish running
+        int priority;           // priority of different tasks
+
+        int arrivaltime;        // determine the time of arrival of the given task, which will affect the remaining time of other tasks
+        int arrivalq;           // indicates the order of arrival of tasks, not considering the specific time of arrival. used for FCFS
+
+        int waitT;
+        int cyclingT;
+        int responseT;
+
+        bool IO;                // whether the task used I/O devices
+        int CPUT;               // cpu time
+        int IOT;                // I/O time
+    };
+
+
+    // simulation info //
+
+    int Tprocess;               // total number of tasks/processes
+    schedulingAlgos algorithm;  // selected scheduling algorithm
+    std::vector<task*> pqueue;  // contain all tasks
+    std::vector<task*> exeQ;    // in the order of execution        // maybe not necessary
+
+    // user experience determinants //
+    int Twait;                  // total waiting time - not necessary
+    int AverWait;               // averge waiting time
+    int MaxWait;
+    int MinWait;
+    int TResp;                  // total response time
+    int AverResp;
+    int MaxResp;
+    int MinResp;
+    int TCy;                    // average cycling time
+    int AverCy;
+    int MaxCy;
+    int MinCy;
+
+    // system efficiency determinants //
+    float CPU_rate;             // percentage of time when cpu is occupied
+    int throughput;             // tasks finished per unit time
+
+
+    // use a heap data structure to maintain the order of the task queue
+    // call cmp when need to compare task attributesd individually -- readability
+    // return whether task a has higher priority compared to task b
+    bool cmpArrivalq(task *, task *);
+    bool cmpRemain(task *, task *);
+    bool cmpArrivalt(task *, task *);
+    bool cmpPriority(task *, task *);
+    bool cmpwaitT(task *, task *);
+
+    bool comparision(task *, task *);
+
+
+    /*
+     * sort the pqueue in to partially ordered tree according to priority
+     */
+    void sortQueue();
+
+    /*
+     * enqueue tasks while keep the partial ordered tree structure
+     */
+    void enqueue(task*);
+
+    /*
+     * Average Waiting Time Calculation
+     */
+    int avgWaitingT();
+
+    /*
+     * calculate efficiency determinants based on execution queue generated
+     * by the selected scheduling algorithm
+     */
+    void efficiency();
+
+    /*
+     * illegalize copying
+     */
+    scheduling & operator=(const scheduling &);
+};
+
 
 
 #endif // SCHEDULING_ALGO_H
