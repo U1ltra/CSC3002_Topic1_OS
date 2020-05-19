a70126a63017f08c6b955751b4df31dca10f8dbc
EndlessSummers <56576107+EndlessSummers@users.noreply.github.com>
2020-05-19 20:28:35 +0800

    Add files via upload

--- memoryManagement/main.cpp
+++ memoryManagement/main.cpp
@@ -3,19 +3,24 @@
 #include <list>
 #include <pair.h>
 #include <Buddy.h>
+#include <m_task.h>
 using namespace std;
 
 int main(){
     int ini = 128;
     Buddy obj = Buddy(128);
-    obj.allocate(16);
-    obj.allocate(16);
-    obj.allocate(16);
-    obj.allocate(16);
-    obj.deallocate(0);
-    obj.deallocate(9);
-    obj.deallocate(32);
-    obj.deallocate(16);
+    m_task task1(10086,15);
+    m_task task2(10010,16);
+    m_task task3(11111,16);
+    m_task task4(11123,16);
+    obj.allocate(task1);
+    obj.allocate(task2);
+    obj.allocate(task3);
+    obj.allocate(task4);
+    obj.deallocate(task1);
+    obj.deallocate(task3);
+    obj.deallocate(task2);
+
     return 0;
 }
 
